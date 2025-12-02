4b4c510f301e6e318261160e96c96e47e9d0ef2e
zidanoye <zdnpoke18@gmail.com>
2025-12-02 22:47:41 +0700

    Add files via upload

---
+++ Praktikum 3/ifelseif.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Demo</title>
+</head>
+<body>
+
+<?php  
+echo 'Kode PHP di sini'; // ...
+?>  
+
+<p> Dokumen HTML </p>
+
+<?php  
+echo 'Kode PHP di sini'; // ...
+?>  
+
+</body>
+</html>
